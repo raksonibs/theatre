8c14ec42221a378de58fcad934212f17be5eb1e9
Oskar Niburski <oskarniburski@gmail.com>
2013-11-27 15:44:22 -0500

    Problem solved

--- models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-	has_secure_password
-	has_many :reviews
-	has_many :events, through: :reviews
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/masks.jpg"
-end
